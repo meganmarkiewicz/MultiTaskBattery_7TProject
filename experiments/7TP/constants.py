ed40144fec658c97495bd2d2ad4b06fc3d9ba166
Bassel Arafat <118376927+Barafat2@users.noreply.github.com>
2025-03-20 13:23:29 -0400

    Update constants.py

--- experiments/7TP/constants.py
+++ experiments/7TP/constants.py
@@ -23,7 +23,7 @@ run_dir    = exp_dir / "run_files"     # contains run files for each session
 data_dir   = exp_dir / "data"          # This is where the result files are being saved
 
 # do run_file_name as a formated string
-default_run_filename = 'run_01.tsv'
+default_run_filename = 'run_{}.tsv'
 
 # This is were the stimuli for the different task are stored
 package_dir = Path(__file__).resolve().parents[2]
