c3b7a7ae06b367cac6cd816870a96ea39699cf23
meganmarkiewicz <markiewicz@kennedykrieger.org>
2025-04-29 12:10:11 -0400

    Update constants.py

--- experiments/7TP/constants.py
+++ experiments/7TP/constants.py
@@ -37,6 +37,6 @@ debug = False                                           # set to True for debugg
 
 # Screen settings for subject display
 screen = {}
-screen['size'] = [2736, 1824]        # screen resolution
-screen['fullscr'] = True          # full screen, if false it's in a separate window
-screen['number'] = 0                # 0 = main display, 1 = secondary display
+screen['size'] = [1100, 800]        # screen resolution
+screen['fullscr'] = False        # full screen, if false it's in a separate window
+screen['number'] = 1                # 0 = main display, 1 = secondary display
